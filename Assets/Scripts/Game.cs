cd2b5990284d0d216491b8f231ce3782e4fca220
James Attard <james@obsidian>
2021-09-29 13:13:48 +0200

    set positions for all chesspieces on the board

--- Assets/Scripts/Game.cs
+++ Assets/Scripts/Game.cs
@@ -19,6 +19,47 @@ public class Game : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        Instantiate(chesspiece, new Vector3(0,0,-1), Quaternion.identity);   
+        // Instantiate(chesspiece, new Vector3(0,0,-1), Quaternion.identity);
+
+        // Instantiate white chesspieces 
+        playerWhite = new GameObject[] { Create("white_rook", 0, 0), Create("white_knight", 1, 0),
+            Create("white_bishop", 2, 0), Create("white_queen", 3, 0), Create("white_king", 4, 0),
+            Create("white_bishop", 5, 0), Create("white_knight", 6, 0), Create("white_rook", 7, 0),
+            Create("white_pawn", 0, 1), Create("white_pawn", 1, 1), Create("white_pawn", 2, 1),
+            Create("white_pawn", 3, 1), Create("white_pawn", 4, 1), Create("white_pawn", 5, 1),
+            Create("white_pawn", 6, 1), Create("white_pawn", 7, 1) };
+
+        // Instantiate black chesspieces 
+        playerBlack = new GameObject[] { Create("black_rook", 0, 7), Create("black_knight",1,7),
+            Create("black_bishop",2,7), Create("black_queen",3,7), Create("black_king",4,7),
+            Create("black_bishop",5,7), Create("black_knight",6,7), Create("black_rook",7,7),
+            Create("black_pawn", 0, 6), Create("black_pawn", 1, 6), Create("black_pawn", 2, 6),
+            Create("black_pawn", 3, 6), Create("black_pawn", 4, 6), Create("black_pawn", 5, 6),
+            Create("black_pawn", 6, 6), Create("black_pawn", 7, 6) };
+
+        // Set all piece positions on the position board
+        for (int i = 0; i < playerBlack.Length; i++)
+        {
+            SetPosition(playerBlack[i]);
+            SetPosition(playerWhite[i]);
+        }
+    }
+
+    public GameObject Create(string name, int x, int y)
+    {
+        GameObject obj = Instantiate(chesspiece, new Vector3(0,0,-1), Quaternion.identity);
+        Chessman cm = obj.GetComponent<Chessman>();
+        cm.name = name;
+        cm.SetXBoard(x);
+        cm.SetYBoard(y);
+        cm.Activate();
+        return obj;
+    }
+
+    public void SetPosition(GameObject obj)
+    {
+        Chessman cm = obj.GetComponent<Chessman>();
+
+        positions[cm.GetXBoard(), cm.GetYBoard()] = obj;
     }
 }
